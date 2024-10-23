f4936df0ec47bf9fc45c81a62ad4edb322885b27
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-23 17:27:37 +0200

    create and use featured carriers section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
-import FeaturesBanner from "../components/comet/features-banner/FeaturesBanner";
+import CometFeaturesBanner from "../components/comet/features-banner/CometFeaturesBanner";
+import DashboardSectionTitle from "../components/common/DashboardSectionTitle";
+import FeaturedCarriers from "../components/ui/FeaturedCarriers";
 import PageHeader from "../components/ui/PageHeader";
 import { HOME_PAGE_MAIN_FEATURES_BANNER_DATA } from "../constants/features-banner";
 
@@ -15,16 +17,34 @@ export default function HomePage() {
     // TODO: Navigate to the customers list page.
   };
 
+  const handleCarrierButtonClick = () => {
+    console.log("Carrier button clicked");
+    // TODO: Navigate to the carriers list page.
+  };
+
   return (
     <>
       <div className="">
         <div className="mt-4 mb-10">
           <PageHeader {...HOME_PAGE_HEADER_DATA} />
         </div>
-        <FeaturesBanner
-          {...HOME_PAGE_MAIN_FEATURES_BANNER_DATA}
-          onClick={handleFeatureBannerButtonClick}
-        />
+        <div className="mb-10">
+          <CometFeaturesBanner
+            {...HOME_PAGE_MAIN_FEATURES_BANNER_DATA}
+            onClick={handleFeatureBannerButtonClick}
+          />
+        </div>
+        <div className="mb-10">
+          <div className="mb-4">
+            <DashboardSectionTitle
+              title="Featured Carriers"
+              description="Explore our top-rated insurance providers, known for their excellent coverage options and customer service."
+              buttonText="See all"
+              onClick={handleCarrierButtonClick}
+            />
+          </div>
+          <FeaturedCarriers />
+        </div>
       </div>
     </>
   );
