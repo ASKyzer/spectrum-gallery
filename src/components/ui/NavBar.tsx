36e8bb23e6cd753121e06befc162f07a82926d1c
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-23 14:43:01 +0200

    move navbar and delete not needed components

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-between items-center h-16 ml-10">
-      <h1 className="text-3xl font-bold text-gray-600">Spectrum Gallery</h1>
+    <div className="flex justify-between items-center h-16 px-4 bg-white border-b border-gray-200 shadow-sm">
+      <h1 className="text-3xl font-bold text-gray-600">
+        <Link to="/">InsureGuard</Link>
+      </h1>
       <nav className="flex items-center gap-4 text-lg font-semibold text-gray-600 mr-10">
         <ul className="flex items-center gap-4">
           <li>
