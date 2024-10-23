a94573556e300f364b392326c67eb4f30357f000
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-23 14:44:46 +0200

    add page header component

---
+++ src/components/ui/PageHeader.tsx
@@ -0,0 +1,18 @@
+import "./PageHeader.css";
+
+export type PageHeaderProps = {
+  title: string;
+  description: string;
+};
+
+export default function PageHeader({
+  title = "Page Title",
+  description = "Enter the description of the page here.",
+}: PageHeaderProps) {
+  return (
+    <div>
+      <h1 className="title">{title}</h1>
+      <p className="description">{description}</p>
+    </div>
+  );
+}
